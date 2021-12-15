77f3fa1729effa32f24c5916bd6605f945030440
Carlos <ccetina4@gmail.com>
2021-12-15 16:33:18 -0500

    Step 5

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,13 +5,15 @@ namespace ConsoleApp
 {
     class Program
     {
+        //program Iterate list without using ForEach/For loops
         static void Main(string[] args)
         {
-            
+
+
             Console.WriteLine("Iterate list \n");
             int count = 0;
             Console.WriteLine("**********************BEGIN************************");
-            while (count!= QuestionClass.NamesList.Count)
+            while (count != QuestionClass.NamesList.Count)
             {
                 Console.WriteLine(QuestionClass.NamesList[count]);
                 count++;
@@ -20,15 +22,9 @@ namespace ConsoleApp
             Console.WriteLine("\n Iterate Finish");
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
-
-
-
-
         }
-
-
-
     }
+
     public static class QuestionClass
     {
         public static List<string> NamesList = new List<string>()
@@ -37,5 +33,67 @@ namespace ConsoleApp
             "Jeffrey",
             "John",
         };
+    }
+
+
+    public class TEST
+    {
+        public  string TESTModule(object value)
+        {
+            string result = string.Empty;
+            try
+            {                
+                if (value != null)
+                {
+                    Type TO = value.GetType();
+                    switch (TO.Name)
+                    {
+                        case "Int32":
+                            if ((Int32)value < 1)
+                            {
+                                result = "Error: you cannot perform operations with the value less than 1";
+                                //throw new Exception("Error: you cannot perform operations with the value less than 1");
+                               
+                            }
+                            if ((Int32)value >= 1 && (Int32)value <= 4)
+                            {
+                                result = ((Int32)value * 2).ToString();                                
+                                break;
+                            }
+                            if ((Int32)value >= 4)
+                            {
+                                result = ((Int32)value * 3).ToString();
+                                break;
+                            }
+
+                            break;
+                        case "Single":
+                            if ((float)value == 1.0f || (float)value == 2.0f)
+                            {
+                                result = "3.0f";
+                            }
+                            break;
+                        case "String":
+                            result = value.ToString().ToUpper();
+                            break;
+                        default:
+                            result =  value.ToString();
+                            break;
+                    }
+                    
+                }
+                else
+                {
+                    throw new Exception("Error TESTModule: value null");
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error TESTModule: " + ex.Message);
+            }
+            return result;
+        }
+
     }
 }
--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -1,3 +1,4 @@
+using ConsoleApp;
 using NUnit.Framework;
 using VogCodeChallenge.API.Business;
 
@@ -21,6 +22,23 @@ namespace UnitTestProject
             Assert.IsNotNull(mng.list_Departments_Filter(1));
             Assert.IsNotNull(mng.list_Departments_Filter(2));
             Assert.IsNotNull(mng.list_Departments_Filter(3));
+        }
+
+        [Test]
+        public void TESTModule1()
+        {
+            TEST mng = new TEST();
+            Assert.AreEqual("2",mng.TESTModule(1));
+            Assert.AreEqual("4", mng.TESTModule(2));
+            Assert.AreEqual("6", mng.TESTModule(3));
+            Assert.AreEqual("8", mng.TESTModule(4));
+            Assert.AreEqual("15", mng.TESTModule(5));
+            Assert.AreEqual("Error: you cannot perform operations with the value less than 1", mng.TESTModule(0));
+            Assert.AreEqual("3.0f", mng.TESTModule(1.0f));
+            Assert.AreEqual("3.0f", mng.TESTModule(2.0f));
+            Assert.AreEqual("", mng.TESTModule(5.0f));
+            Assert.AreEqual("CARLOS", mng.TESTModule("carlos"));
+            Assert.AreEqual("True", mng.TESTModule(true));
         }
     }
 }
